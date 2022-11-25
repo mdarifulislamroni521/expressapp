46bf8c628a2bf357180f28ae354002893c1f8ccd
mdarifulislamroni521 <mdarifulislamroni521@gmail.com>
2022-11-25 22:45:24 +0600

    3rd

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.all("/", (req, res) => {
   res.status(200).json({ ...req.headers, visitTime: Date(Date.now()) });
 });
 
-app.listen({ port: 80 }, () => {
+app.listen({ port: 8000 }, () => {
   console.log("started app on 8383 port success");
 });
