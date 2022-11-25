de893b5fd250d1233b2f6863d71a750debd293d2
mdarifulislamroni521 <mdarifulislamroni521@gmail.com>
2022-11-25 22:23:01 +0600

    test project startted

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+// app method
+app.all("/", (req, res) => {
+  res.status(200).json({ ...req.headers, visitTime: Date(Date.now()) });
+});
+
+app.listen({ port: 8383 }, () => {
+  console.log("started app on 8383 port success");
+});
