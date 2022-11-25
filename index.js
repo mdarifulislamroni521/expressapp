420eea825c7de910ae1da3536981aabb92518c30
mdarifulislamroni521 <mdarifulislamroni521@gmail.com>
2022-11-25 22:30:31 +0600

    port 80 setup

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.all("/", (req, res) => {
   res.status(200).json({ ...req.headers, visitTime: Date(Date.now()) });
 });
 
-app.listen({ port: 8383 }, () => {
+app.listen({ port: 80 }, () => {
   console.log("started app on 8383 port success");
 });
